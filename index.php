0361733b58fa5bda7cf90d672e6c2e819517141c
sergiosanchez71 <sergiosanalv@gmail.com>
2020-04-01 21:01:27 +0200

    Primer commit

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
         <script src="controlador/js/libreriaJQuery.js" type="text/javascript"></script>
         <script>
 
+            alert("Borrar este mensaje");
+
             $(document).ready(function () {
                 inicio();
             });
