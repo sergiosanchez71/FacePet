9c6a74d890b3c0aef4e94b891994dacc8e9cd939
sergiosanchez71 <sergiosanalv@gmail.com>
2023-08-26 13:08:10 +0200

    New conexion database

--- controlador/clases/Conexion.php
+++ controlador/clases/Conexion.php
@@ -11,13 +11,22 @@
  *
  * @author Sergio Sánchez
  */
+
+
+include '../../conf/define.php';
+
+$servername = DB_HOST;
+$username = USER;
+$password = PASS;
+$database = DATABASE;
+
 class Conexion {
 
     //Clase para acceder a la base de datos
     //Nuestro fichero con el que nos conectaremos a la base de datos
     public static function conectar() {
         try {
-            $gbd = new PDO('mysql:host=localhost;dbname=facepet', 'root', '');
+            $gbd = new PDO('mysql:host='.DB_HOST.';dbname='.DATABASE, USER, PASS);
             return $gbd;
         } catch (PDOException $e) {
             echo 'Falló la conexión: ' . $e->getMessage();
